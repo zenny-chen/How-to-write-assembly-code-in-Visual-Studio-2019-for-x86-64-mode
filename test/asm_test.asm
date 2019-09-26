01b10523d063ac56a46416b905e38d533d528551
Zenny Chen <zenny_chen@sina.com>
2019-09-27 05:15:15 +0800

    Add files via upload

--- test/asm_test.asm
+++ test/asm_test.asm
@@ -2,6 +2,8 @@
 
 .code
 
+extern MyCAdd : proc
+
 ; 将第一个参数减去第二个参数的值进行返回
 MyASMSub     proc public
 
@@ -10,6 +12,22 @@ MyASMSub     proc public
     ret
 
 MyASMSub     endp
+
+
+MyASMCallCTest  proc public
+
+    sub     rsp, 64             ; 确保对MyCAdd函数调用的栈帧不受破坏
+    mov     [rsp + 60], ecx     ; 先将传递进来的参数n保存在栈上（安全起见，保存在当前函数栈空间的最顶部）
+
+    mov     ecx, 3
+    mov     edx, 4
+    call    MyCAdd              ; 调用C语言实现的加法函数，实现3+4的操作
+
+    sub     eax, [rsp + 60]     ; 将计算结果与参数n相减
+    add     rsp, 64
+    ret
+
+MyASMCallCTest endp
 
 
 end
--- test/main.c
+++ test/main.c
@@ -5,14 +5,26 @@
 
 #include <stdio.h>
 
-/* return the value of a - b */
+/* 返回a - b的差值 */
 extern int MyASMSub(int a, int b);
+
+/* 在汇编过程中实现求和操作，再减去n返回差值 */
+extern int MyASMCallCTest(int n);
+
+/* 在C语言端实现的全局加法函数。它将被汇编语言过程所调用。 */
+int MyCAdd(int a, int b)
+{
+    return a + b;
+}
 
 int main(int argc, const char * argv[])
 {
     // insert code here...
-    const int result = MyASMSub(8, 3);
+    int result = MyASMSub(8, 3);
     printf("The reuslt is: %d\n", result);
+
+    result = MyASMCallCTest(5);
+    printf("The result is: %d\n", result);
     
     return 0;
 }
